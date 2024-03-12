21ede0770df2b2ad2c7aaac5c8cf12fb574a4294
RonK-0 <ronkua12@gmail.com>
2024-03-12 14:05:29 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const toggleMenu = document.querySelector("#toggleMenu");
+const dropdown = document.querySelector("#menu");
+const body = document.querySelector("body");
+
+toggleMenu.addEventListener("click", () => {
+    dropdown.classList.toggle("open");
+    body.classList.toggle("open");
+});
+
+const btnTheme = document.querySelector('.theme');
+const root = document.querySelector(':root');
+// const body = document.querySelector('body');
+const icon = document.querySelector(".far");
+
+btnTheme.addEventListener("click", () => {
+  if(body.classList.contains('dark')){
+    body.classList.remove('dark');
+    root.style.setProperty('--light', '#fff');
+    root.style.setProperty('--dark', '#000');
+    icon.classList.remove("fa-sun");
+    icon.classList.add("fa-moon");
+  }
+  else{
+    body.classList.add('dark');
+    root.style.setProperty('--light', '#000');
+    root.style.setProperty('--dark', '#fff');
+    icon.classList.add("fa-sun");
+    icon.classList.remove("fa-moon");
+  }
+});
