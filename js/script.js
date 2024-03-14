ec174f425d6c3e703b282abf5e684e51b713c4db
RonK-0 <ronkua12@gmail.com>
2024-03-14 09:43:03 +0800

    header, banner, footer, cta done

--- js/script.js
+++ js/script.js
@@ -7,26 +7,26 @@ toggleMenu.addEventListener("click", () => {
     body.classList.toggle("open");
 });
 
-const btnTheme = document.querySelector('.theme');
-const root = document.querySelector(':root');
-// const body = document.querySelector('body');
-const icon = document.querySelector(".far");
+// const btnTheme = document.querySelector('.theme');
+// const root = document.querySelector(':root');
+// // const body = document.querySelector('body');
+// const icon = document.querySelector(".far");
 
-btnTheme.addEventListener("click", () => {
-  if(body.classList.contains('dark')){
-    body.classList.remove('dark');
-    root.style.setProperty('--light', '#fff');
-    root.style.setProperty('--dark', '#000');
-    root.style.setProperty('--gray', '#222');
-    icon.classList.remove("fa-sun");
-    icon.classList.add("fa-moon");
-  }
-  else{
-    body.classList.add('dark');
-    root.style.setProperty('--light', '#333');
-    root.style.setProperty('--dark', '#fff');
-    root.style.setProperty('--gray', '#ccc');
-    icon.classList.add("fa-sun");
-    icon.classList.remove("fa-moon");
-  }
-});
+// btnTheme.addEventListener("click", () => {
+//   if(body.classList.contains('dark')){
+//     body.classList.remove('dark');
+//     root.style.setProperty('--light', '#fff');
+//     root.style.setProperty('--dark', '#000');
+//     root.style.setProperty('--gray', '#222');
+//     icon.classList.remove("fa-sun");
+//     icon.classList.add("fa-moon");
+//   }
+//   else{
+//     body.classList.add('dark');
+//     root.style.setProperty('--light', '#333');
+//     root.style.setProperty('--dark', '#fff');
+//     root.style.setProperty('--gray', '#ccc');
+//     icon.classList.add("fa-sun");
+//     icon.classList.remove("fa-moon");
+//   }
+// });
